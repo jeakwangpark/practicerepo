1e113f1a6aed057ffbf85de5c690167ddecbd4a7
Open Source SW <pjk163344@naver.com>
2025-12-07 09:06:33 +0000

    New line about log

--- newfeature.c
+++ newfeature.c
@@ -1,12 +1,18 @@
+
+
 #include <stdio.h>
 #include <unistd.h>
 
 void main(){
 
  char pwd[1024];
+ FILE *f = fopen("pwd-log.log",'w');
  getcwd(pwd, sizeof(pwd));
 
  printf("%s\n", pwd);
+ fputs("%s\n",f);
+ fclose(f);
+
 
 
 }
